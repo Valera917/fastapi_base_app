e4894cca9f3e7fd153b9ad0cd673659e6167f152
Valera <v.medovkin1664@gmail.com>
2024-06-11 23:21:21 +0300

    use run settings

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -4,4 +4,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.api.host,
+        port=settings.api.port,
+        reload=True,
+    )
